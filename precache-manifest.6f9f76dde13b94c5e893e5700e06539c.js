437f23e605e0954f811c8fc72b89bd8b336e2c95
nofar-ch <nofar.badihi@gmail.com>
2020-09-09 15:43:53 +0300

    Updates

---
+++ precache-manifest.6f9f76dde13b94c5e893e5700e06539c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99475e327cf534a39e37e42b0fb2aab",
+    "url": "/serversManagement/index.html"
+  },
+  {
+    "revision": "8ead849fe9410e0e8925",
+    "url": "/serversManagement/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30bd588e36a8f61fee5d",
+    "url": "/serversManagement/static/js/2.56ee485c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/serversManagement/static/js/2.56ee485c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ead849fe9410e0e8925",
+    "url": "/serversManagement/static/js/main.9bef504b.chunk.js"
+  },
+  {
+    "revision": "052dd7d1e2ed2eded6f2",
+    "url": "/serversManagement/static/js/runtime-main.6e12c343.js"
+  }
+]);
